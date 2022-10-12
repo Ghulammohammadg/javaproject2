1cd7136157509728071b6926dc4a1858c6b5b2fa
Ghulammohammadg <105859349+Ghulammohammadg@users.noreply.github.com>
2022-10-12 18:53:23 +0430

    java file

---
+++ A.java
@@ -0,0 +1,22 @@
+package packege4;
+
+public class A {
+	void method1() {
+		System.out.println("this is methed1");
+	}void method2() {
+		System.out.println("this is methed12");
+	}
+	
+	
+
+}
+class B extends A{
+	
+	
+	void method3() {
+		System.out.println("this is methed3");
+	}
+	void method4() {
+		System.out.println("this is methed4");
+	}
+}
---
+++ main.java
@@ -0,0 +1,15 @@
+package packege4;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		A obj=new A(); //here is tow methods
+	
+		B obj1=new B();//here is four methods
+		obj1.m
+	
+	}
+
+}
